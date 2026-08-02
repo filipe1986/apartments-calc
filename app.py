57b261a03e87b169919186ba048c959a7cb09081
Filipe <piracicaba@gmail.com>
2026-08-01 22:18:22 -0300

    adding stats model on requirements

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from pathlib import Path
+import statsmodels
 
 st.set_page_config(
     page_title="Apartments Portfolio Dashboard",
